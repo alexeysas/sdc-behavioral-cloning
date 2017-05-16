7dfa46f4c59ddd5cebf2ecfbd25baff5c4db0cd0
Alexey Sas <asasryl@gmail.com>
2017-05-16 00:45:32 -0500

    some stuff

--- main.py
+++ main.py
@@ -2,69 +2,87 @@ import csv
 import numpy as np
 import cv2
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Activation, Lambda, Dropout, Cropping2D
+from keras.layers import Dense, Flatten, Lambda, Dropout, Cropping2D
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
 
-def load_samples(file_name, test_size=0.2, ignore_threshold=0.1):
-    """Load collected data reference and create train and validation set randomly shuffled"""
+def load_samples(file_name, test_size=0.2, ignore_threshold=0.1, remove_probability=0.7):
+    """ Load collected data reference and create train and validation set randomly shuffled
+    ignore_threshold - angles less than this value are ignored
+    and not added to the dataset with probability: remove_probability
+    """
     lines = []
     with open(file_name) as csv_file:
         reader = csv.reader(csv_file)
         for line in reader:
             mesurement = float(line[3])
-            if (np.abs(mesurement) > ignore_threshold) or (np.random.rand() > 0.7):
+            if (np.abs(mesurement) > ignore_threshold) or (np.random.rand() > remove_probability):
+                #if mesurement != 0: 
                 lines.append(line)
-    train_samples, validation_samples = train_test_split(lines, test_size=test_size, random_state = 5)
+    train_samples, validation_samples = train_test_split(lines, test_size=test_size)
     return train_samples, validation_samples
 
-
-def region_of_interest(img, vertices):
-    """
-    Applies an image mask.
-    
-    Only keeps the region of the image defined by the polygon
-    formed from `vertices`. The rest of the image is set to black.
-    """
-    #defining a blank mask to start with
-    mask = np.zeros_like(img)   
-    
-    #defining a 3 channel or 1 channel color to fill the mask with depending on the input image
-    if len(img.shape) > 2:
-        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
-        ignore_mask_color = (255,) * channel_count
-    else:
-        ignore_mask_color = 255
-        
-    #filling pixels inside the polygon defined by "vertices" with the fill color    
-    cv2.fillPoly(mask, vertices, ignore_mask_color)
-    
-    #returning the image only where mask pixels are nonzero
-    masked_image = cv2.bitwise_and(img, mask)
-    return masked_image
-
-
 def load_image(file_name):
-    """Load image from disk"""
+    """ Load image from disk """
     image = cv2.imread(file_name)
     return image
 
+#def prepare_image(image):
+#    """Load image from disk"""
+#    vertices = np.array([[(0, 140), (0, 50), (320, 50), (320, 140)]])
+#    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#    image = region_of_interest(image, vertices)
+#    return image
+#    img_out = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+#    img_out = cv2.bilateralFilter(image,15,75,75)
+#
+#    return img_out
 
-def prepare_image(image):
-    """Load image from disk"""    
-    vertices = np.array([[(0, 130), (0, 70), (320, 70), (320, 130)]])
-    image = region_of_interest(image, vertices)
-    return image
-    img_out = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-    img_out = cv2.bilateralFilter(image,15,75,75)
-    
-    return img_out
+def get_augumented_images(line, get_image=load_image, 
+        use_additional_cameras=False, coof1=1, coof2=0.25):
+    """ Create augumented images based on input image and angle, base version just flips image and add one additional image
+    get_image - function to load image
+    use_additional_cameras - indicate if we need to use images from
+        right and left cameras in addition to central image
+    coof1, coof2 - left and right camera angle adjustment using angle * coof1 + coof2
+        or angle * coof1 - coof2
+    """
+    images = []
+    angles = []
+    image = get_image(line[0])
+    angle = float(line[3])
 
-    
+    #add base image
+    images.append(image)
+    angles.append(angle)
+
+    #add flipped image
+    images.append(cv2.flip(image, 1))
+    angles.append(-angle)
+
+    if use_additional_cameras:
+        steering_left = coof1 * angle + coof2
+        steering_right = 1.0 / coof1 * angle - coof2
+
+        #add images from left camera
+        image = get_image(line[1])
+        images.append(image)
+        angles.append(steering_left)
+        images.append(cv2.flip(image, 1))
+        angles.append(-steering_left)
+
+        #add images from right camera
+        image = get_image(line[2])
+        images.append(image)
+        angles.append(steering_right)
+        images.append(cv2.flip(image, 1))
+        angles.append(-steering_right)
+    return images, angles
 
 def generator(samples, batch_size, get_image):
+    """ Generator based trainig in case we have a lot of images which are not fit to memory """
     num_samples = len(samples)
     factor = 2 # we have augumented images
     internal_batch_size = batch_size // factor
@@ -89,45 +107,53 @@ def generator(samples, batch_size, get_image):
             y_train = np.array(angles)
             yield shuffle(x_train, y_train)
 
-def load_image_data(samples, get_image=load_image):
-    """ preprocess image and mesurement data """
+def load_image_data(samples, get_image=load_image,
+        use_additional_cameras=False, coof1=1, coof2=0.25):
+    """ Load and preprocess image and angles data
+    get_image - function to load image
+    use_additional_cameras - indicate if we need to use images from
+        right and left cameras in addition to central image
+    coof1, coof2 - left and right camera angle adjustment using angle * coof1 + coof2
+        or angle * coof1 - coof2
+    """
     images = []
     angles = []
 
     for line in samples:
-        image = get_image(line[0])
-        #image = prepare_image(image)
-        mesurement = float(line[3])
-        images.append(image)
-        angles.append(mesurement)
-        images.append(cv2.flip(image, 1))
-        angles.append(-mesurement)
-
-        correction_b = 0.1
-        correction_a = 1
-
-        #steering_left = correction_a * mesurement + correction_b
-        #steering_right = correction_a * mesurement - correction_b
-
-        #image = get_image(line[1])
-        #images.append(image)
-        #angles.append(steering_left)
-        #images.append(cv2.flip(image, 1))
-        #angles.append(-steering_left)
-
-        #image = get_image(line[2])
-        #images.append(image)
-        #angles.append(steering_right)
-        #mages.append(cv2.flip(image, 1))
-        #angles.append(-steering_right)
-
+        new_images, new_angles = get_augumented_images(line, get_image=get_image,
+                use_additional_cameras=use_additional_cameras, coof1=coof1, coof2=coof2)
+        images.extend(new_images)
+        angles.extend(new_angles)
     return np.array(images), np.array(angles)
 
-def create_model():
-    """ build model """
+
+def get_base_model():
+    """ Create base model template with pre-processing layers """
     model = Sequential()
-    model.add(Cropping2D(cropping=((50, 20), (0, 0)), input_shape=(160, 320, 3)))
+    model.add(Cropping2D(cropping=((50, 20), (20, 20)), input_shape=(160, 320, 3)))
     model.add(Lambda(lambda x: x / 255.0 - 0.5))
+    return model
+
+def create_lenet_model():
+    """ Build LeNeT model """
+    model = get_base_model()
+    model.add(Convolution2D(6, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(16, 5, 5, activation="relu"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dropout(0.8))
+    model.add(Dense(84))
+    model.add(Dropout(0.8))
+    model.add(Dense(1))
+    model.compile(loss='mse', optimizer='adam')
+
+    return model
+
+def create_nvidia_model():
+    """ build NVIDIA model archirecture """
+    model = get_base_model()
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
     model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation="relu"))
@@ -143,6 +169,23 @@ def create_model():
     model.compile(loss='mse', optimizer='adam')
 
     return model
+
+def show_angles_hist(angles):
+    """ Utility method to show collected angles distribution """
+    xbins = 20
+    plt.hist(angles, bins=xbins, color='blue')
+    plt.show()
+
+def show_train_and_validation_loss(history_object):
+    """ Utility method to display model loss  """
+    plt.plot(history_object.history['loss'])
+    plt.plot(history_object.history['val_loss'])
+    plt.title('model mean squared error loss')
+    plt.ylabel('mean squared error loss')
+    plt.xlabel('epoch')
+    plt.legend(['training set', 'validation set'], loc='upper right')
+    plt.show()
+   
 
 def main_generator():
     """ preprocess image data and train model """
@@ -176,34 +219,30 @@ def main():
     epochs = 10
 
     csv_log = "data/driving_log.csv"
-    train_data, validation_data = load_samples(csv_log, 0.1, 0.3)
+    train_data, validation_data = load_samples(csv_log, 0.1, 0.3, remove_probability=1)
 
-    #image = load_image(train_data[0][0])
+    image = load_image(train_data[0][0])
     #plt.imshow(image)
     #plt.show()
 
     #image = prepare_image(image)
     #plt.imshow(image)
     #plt.show()
-    #return
-    print ("Load image data")
 
-    train_x, train_y = load_image_data(train_data)
-    validation_x, validation_y = load_image_data(validation_data)
-    
-    model = create_model()
+    print("Loading image data...")
 
-    history_object = model.fit(train_x, train_y, batch_size=batch_size, epochs=epochs, verbose=1, validation_data = (validation_x, validation_y))
+    train_x, train_y = load_image_data(train_data, use_additional_cameras=False, coof1=1, coof2=0.25)
+    validation_x, validation_y = load_image_data(validation_data, use_additional_cameras=False)
+
+    show_angles_hist(train_y)
+    model = create_nvidia_model()
+
+    history_object = model.fit(train_x, train_y, batch_size=batch_size, 
+            epochs=epochs, verbose=1, validation_data = (validation_x, validation_y))
 
     model.save('model.h5')
 
-    plt.plot(history_object.history['loss'])
-    plt.plot(history_object.history['val_loss'])
-    plt.title('model mean squared error loss')
-    plt.ylabel('mean squared error loss')
-    plt.xlabel('epoch')
-    plt.legend(['training set', 'validation set'], loc='upper right')
-    plt.show()
+    show_train_and_validation_loss(history_object)
     pass
 
 main()
